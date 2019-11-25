4035d3918dfae6ee45c73f8a0f3aa2ffad11d206
andrusha52 <andrushaes@gmail.com>
2019-11-25 14:27:21 +0200

    project

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,19 +2,24 @@
 
 const burger = document.querySelector('.burger-btn');
 const closeBtn = document.querySelector('.section_nav__btn_burger_close');
-const burgerMenu = document.querySelector('.hidden_mobile_menu');
+const burgerMenu = document.querySelector('.hidden_mobile');
+const menuAnchors = [...document.querySelectorAll('.btn_menu')];
 
 const openMenu = e => {
     e.preventDefault();
     burger.style.display = "none";
+    document.body.style.overflow = "hidden";
     burgerMenu.classList.add('hidden_mobile_menu-active');
 }
 
 const closeMenu = e => {
-    e.preventDefault();
     burger.style.display = "block";
+    document.body.style.overflow = "auto";
     burgerMenu.classList.remove('hidden_mobile_menu-active');
 }
 
 burger.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
+menuAnchors.forEach(e => {
+    e.addEventListener('click', closeMenu);
+});
